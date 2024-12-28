6b4785eecde9155c341312f8394b7d1db3bb6aee
Aviral Mehrotra <aviralmehrotra2002@hotmail.com>
2024-12-28 18:36:12 +0530

    changed welcome text

--- client/src/pages/shopping/home.jsx
+++ client/src/pages/shopping/home.jsx
@@ -1,5 +1,38 @@
-function ShoppingHome() {
-  return <div>Shopping Page</div>;
-}
+import { Button } from "@/components/ui/button";
+import bannerOne from "../../assets/banner-1.jpg";
+import bannerTwo from "../../assets/banner-2.jpg";
+import bannerThree from "../../assets/banner-3.jpg";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 
+const slides = [bannerOne, bannerTwo, bannerThree];
+console.log(slides);
+function ShoppingHome() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="relative w-full h-[600px] overflow-hidden">
+        {slides.map((slide, index) => (
+          <img
+            src={slide}
+            key={index}
+            className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-1000`}
+          />
+        ))}
+        <Button
+          variant="outline"
+          size="icon"
+          className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/80"
+        >
+          <ChevronLeftIcon className="w-4 h-4" />
+        </Button>
+        <Button
+          variant="outline"
+          size="icon"
+          className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/80"
+        >
+          <ChevronRightIcon className="w-4 h-4" />
+        </Button>
+      </div>
+    </div>
+  );
+}
 export default ShoppingHome;
